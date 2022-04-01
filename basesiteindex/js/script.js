c4286a5f39109f0ce993f14aa58900b973494af8
Carlos <lopescurtolo@gmail.com>
2022-04-01 17:24:05 -0300

    .

--- basesiteindex/js/script.js
+++ basesiteindex/js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     $(".hamburguer").click(function () {
       $(this).toggleClass("hamburguer-active");
       $('.nav-list-marcas-mobile').toggleClass("active");      
+      $('.nav-list-products-mobile').toggleClass("active");
       $('.nav-list-mobile').toggleClass("active");      
     });
   });
