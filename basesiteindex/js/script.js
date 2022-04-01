beefdb5ce2a2f4b3e6988a2ded735f30ca86f3de
Carlos <lopescurtolo@gmail.com>
2022-04-01 19:49:13 -0300

    fixing'n'improving

--- basesiteindex/js/script.js
+++ basesiteindex/js/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
     $(".hamburguer").click(function () {
       $(this).toggleClass("hamburguer-active");
-      $('.nav-list-marcas-mobile').toggleClass("active");      
+      $('.nav-list-mobile').toggleClass("active");
       $('.nav-list-products-mobile').toggleClass("active");
-      $('.nav-list-mobile').toggleClass("active");      
+      $('.nav-list-marcas-mobile').toggleClass("active");
     });
   });
   
