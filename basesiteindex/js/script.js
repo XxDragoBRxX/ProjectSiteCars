fbef847372e661b19dd51fe3bff66d148d2de512
Carlos <lopescurtolo@gmail.com>
2022-04-01 15:20:03 -0300

    trying animate the brands

--- basesiteindex/js/script.js
+++ basesiteindex/js/script.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
     $(".hamburguer").click(function () {
       $(this).toggleClass("hamburguer-active");
-      $('.nav-list-mobile').toggleClass("active");        
+      $('.nav-list-marcas-mobile').toggleClass("active");      
+      $('.nav-list-mobile').toggleClass("active");      
     });
   });
+  
